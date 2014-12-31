fffacc140c3b95bb48fd031c2b9813c82b16fda0
Dmitry Shechtman <damnian@gmail.com>
2014-12-31 18:35:18 +0200

    Separated AsyncMvvm.Calculated

--- AsyncMvvm.Calculated/Portable/Properties/AssemblyInfo.cs
+++ AsyncMvvm.Calculated/Portable/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Ditto.AsyncMvvm.Calculated")]
-[assembly: AssemblyDescription("AsyncMvvm library CalculatedProperties integration")]
+[assembly: AssemblyDescription("AsyncMVVM library Calculated Properties integration")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
